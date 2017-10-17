11b051a3da6fe3aa6e434e490b95d5aa08e0aed2
unknown <mickaelbenes@hotmail.fr>
2017-10-18 00:24:43 +0200

    Fixed velocity and gravity accordingly.

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,10 @@ import { createMario } from "./entities.js";
 import { loadBackgroundSprites } from "./sprites.js";
 import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 
+window.addEventListener( 'keydown', event => {
+	console.log( event );
+});
+
 const canvas    = document.getElementById( 'screen' );
 const context   = canvas.getContext( '2d' );
 
@@ -18,7 +22,7 @@ Promise.all([
 	const backgroundLayer	= createBackgroundLayer( level.backgrounds, backgroundSprites );
 	comp.layers.push( backgroundLayer );
 
-	const gravity	= 30;
+	const gravity	= 2000;
 
 	mario.pos.set( 64, 180 );
 	mario.vel.set( 200, -600 );
@@ -29,10 +33,9 @@ Promise.all([
 	const timer = new Timer();
 
 	timer.update = function update( deltaTime ) {
-		comp.draw( context );
 		mario.update( deltaTime );
-		console.log( mario.pos );
-		mario.vel.y += gravity;
+		comp.draw( context );
+		mario.vel.y += gravity * deltaTime;
 	}
 
 	timer.start();
