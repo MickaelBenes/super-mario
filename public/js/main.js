807ae4aa3d735a1dd668a0cc3dab993abc444608
unknown <mickaelbenes@hotmail.fr>
2017-10-18 00:18:59 +0200

    End of episode 3.

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,40 @@
 import Compositor from './Compositor.js';
+import Timer from './Timer.js';
 import { loadLevel } from "./loaders.js";
-import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer } from "./layers.js";
+import { createMario } from "./entities.js";
+import { loadBackgroundSprites } from "./sprites.js";
+import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 
 const canvas    = document.getElementById( 'screen' );
 const context   = canvas.getContext( '2d' );
 
-function createSpriteLayer( sprite, pos ) {
-	return function drawSpriteLayer( context ) {
-		for ( let i = 0; i < 20; ++i ) {
-			sprite.draw( 'idle', context, pos.x + i * 16, pos.y );
-		}
-	}
-}
-
 Promise.all([
-	loadMarioSprite(),
+	createMario(),
 	loadBackgroundSprites (),
 	loadLevel( '1-1' )
 ])
-.then(([ marioSprite, backgroundSprites, level ]) => {
+.then(([ mario, backgroundSprites, level ]) => {
 	const comp				= new Compositor();
 	const backgroundLayer	= createBackgroundLayer( level.backgrounds, backgroundSprites );
 	comp.layers.push( backgroundLayer );
 
-	const pos = {
-		x: 0,
-		y: 0
-	};
+	const gravity	= 30;
 
-	const spriteLayer = createSpriteLayer( marioSprite, pos );
+	mario.pos.set( 64, 180 );
+	mario.vel.set( 200, -600 );
+
+	const spriteLayer = createSpriteLayer( mario );
 	comp.layers.push( spriteLayer );
-	
-	function update() {
-		comp.draw( context );
-		pos.x += 2;
-		pos.y += 2;
 
-		requestAnimationFrame( update );
+	const timer = new Timer();
+
+	timer.update = function update( deltaTime ) {
+		comp.draw( context );
+		mario.update( deltaTime );
+		console.log( mario.pos );
+		mario.vel.y += gravity;
 	}
 
-	update();
+	timer.start();
 
 });
---
+++ public/js/math.js
@@ -0,0 +1,13 @@
+export class Vec2 {
+
+	constructor( x, y ) {
+		this.x	= x;
+		this.y	= y;
+	}
+
+	set( x, y ) {
+		this.x	= x;
+		this.y	= y;
+	}
+
+}
