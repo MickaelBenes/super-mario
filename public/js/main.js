b3df2866ff1bfaa29ffbe118f003db989c2b7bae
Mickael Benes <mickaelbenes@hotmail.fr>
2017-11-07 09:08:07 +0100

    Refactored code to spawn entities from JSON. End of ep. 11.

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,22 @@
 import Camera from './Camera.js';
 import Timer from './Timer.js';
 import { setupKeyboard } from './input.js';
-import { loadLevel } from './loaders/level.js';
+import { createLevelLoader } from './loaders/level.js';
 import { loadEntities } from './entities.js';
 
-const canvas    = document.getElementById( 'screen' );
-const context   = canvas.getContext( '2d' );
+async function main( canvas ) {
+	const context = canvas.getContext( '2d' );
 
-Promise.all([
-	loadEntities(),
-	loadLevel( '1-1' )
-])
-.then( ([entity, level] ) => {
+	const entityFactory = await loadEntities();
+	const loadLevel		= await createLevelLoader( entityFactory );
+	const level			= await loadLevel( '1-1' )
+
 	const camera = new Camera();
 	window.camera = camera;
 
-	const mario = entity.mario();
+	const mario = entityFactory.mario();
 	mario.pos.set( 64, 64 );
-
-	const goomba = entity.goomba();
-	goomba.pos.x = 220;
-
-	const koopa = entity.koopa();
-	koopa.pos.x = 260;
-
 	level.entities.add( mario );
-	level.entities.add( goomba );
-	level.entities.add( koopa );
 
 	const input = setupKeyboard( mario );
 	input.listenTo( window );
@@ -44,4 +34,7 @@ Promise.all([
 	}
 
 	timer.start();
-});
+}
+
+const canvas = document.getElementById( 'screen' );
+main( canvas );
